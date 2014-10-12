e86b937f99048122fd32896a678438af1bc3e3cb
ibrahimbaykal <halilibrahimbaykal@gmail.com>
2014-10-12 20:13:00 +0300

    ilk commit

---
+++ istatistikler/TurkiyeRaporlari/src/com/ibrahim/model/BasicPopulationObject.java
@@ -0,0 +1,42 @@
+package com.ibrahim.model;
+
+public class BasicPopulationObject {
+
+	private String erkek;
+	private String kadýn;
+	private String toplam;
+	private String ilAdi;
+
+	public String getErkek() {
+		return erkek;
+	}
+
+	public void setErkek(String erkek) {
+		this.erkek = erkek;
+	}
+
+	public String getKadýn() {
+		return kadýn;
+	}
+
+	public void setKadýn(String kadýn) {
+		this.kadýn = kadýn;
+	}
+
+	public String getToplam() {
+		return toplam;
+	}
+
+	public void setToplam(String toplam) {
+		this.toplam = toplam;
+	}
+
+	public String getIlAdi() {
+		return ilAdi;
+	}
+
+	public void setIlAdi(String ilAdi) {
+		this.ilAdi = ilAdi;
+	}
+
+}
